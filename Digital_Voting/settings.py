7ec324eb955ec0d4780932cc78db14a9bf8b33ed
Yash Patel <yyashhpatel011@gmail.com>
2025-04-03 12:09:32 +0530

    Initial Commit

--- Digital_Voting/settings.py
+++ Digital_Voting/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Digital_Voting',
-        'USER': 'username',
-        'PASSWORD':'password',
+        'USER': 'postgres',
+        'PASSWORD':'Yash@123',
         'HOST':'localhost'
     }
 }
